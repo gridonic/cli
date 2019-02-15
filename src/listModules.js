6cf561f072a84046ddc70e5eeb99cbdfc8d67214
Sebastian Prein <hi@sebastianprein.com>
2019-02-15 15:17:41 +0100

    feat: if module is not found locally, search globally

--- src/listModules.js
+++ src/listModules.js
@@ -1,7 +1,26 @@
 // @see https://github.com/chalk/chalk
 const chalk = require('chalk');
 
+const nameColumn = 25;
+const versionColumn = 12;
+const notFound = 'not found'.padEnd(versionColumn);
+
 // Helper function to format modules consistently during console output
 module.exports = modules => modules
-    .map(({ name, version }) => chalk`{gray ${name} (v${version})}`)
+    .map(({ name, version, global }) => {
+        let location;
+
+        if (global === true) {
+            location = chalk`{yellow global}`;
+        } else {
+            location = chalk`{gray local}`;
+        }
+
+        // If version is null it means the module was not found
+        if (version === null) {
+            return chalk`{red.bold ${name.padEnd(nameColumn)}} {gray ${notFound}}`;
+        }
+
+        return chalk`{gray ${name.padEnd(nameColumn)} ${`${version}`.padEnd(versionColumn)}} ${location}`;
+    })
     .join('\n');
--- src/supportedModules.js
+++ src/supportedModules.js
@@ -1,12 +1,31 @@
 // @see https://github.com/sindresorhus/import-cwd
 const importCwd = require('import-cwd');
 
-// @see https://github.com/gridonic/webpack
-const { cli: webpack } = importCwd('@gridonic/webpack');
-
-// @see https://github.com/gridonic/generator
-// const { cli: generator } = require('@gridonic/generator');
+// @see https://github.com/sindresorhus/import-global
+const importGlobal = require('import-global');
 
 module.exports = [
-    webpack
-];
+
+    // @see https://github.com/gridonic/webpack
+    '@gridonic/webpack',
+
+    // @see https://github.com/gridonic/generator
+    '@gridonic/generator',
+
+].map((module) => {
+    const local = importCwd.silent(module);
+    const global = importGlobal.silent(module);
+
+    if (local === null && global === null) {
+        return {
+            name: module,
+            version: null,
+            commands: []
+        }
+    }
+
+    return {
+        ...(local || global).cli,
+        global: global !== null
+    };
+});
